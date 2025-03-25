fceeca32beccc45bc59dda3c3769264ab8c6c4d4
Ethan <ethanburkett5184@gmail.com>
2025-03-25 17:38:26 -0500

    Update Added new functionality
    added the ability to open jupyter notebook. command "jup"

--- Jarvis.py
+++ Jarvis.py
@@ -1,6 +1,7 @@
 import random
 import webbrowser
 import os
+import subprocess
 
 def print_logo():
     logo = r""" 
@@ -32,7 +33,37 @@ def Jarvis():
     print_face()
 
     # Greeting after printing the logo and face
-    print("Hello, i'm Jarvis. How can I help you today?", flush=True)
+    print("Hello, I'm Jarvis. How can I help you today?", flush=True)
+
+    # Dictionaries for websites and applications
+    websites = {
+        "youtube": "https://www.youtube.com",
+        "yt": "https://www.youtube.com",
+        "google": "https://www.google.com",
+        "gmail": "https://mail.google.com",
+        "github": "https://github.com",
+        "reddit": "https://www.reddit.com",
+        "amazon": "https://www.amazon.com",
+        "stackoverflow": "https://stackoverflow.com",
+        "comptia": "https://login.comptia.org",
+        "chess": "https://www.chess.com/play",
+        "monkeytype": "https://monkeytype.com",
+        "dictionary": "https://www.dictionary.com",
+        "microsoft": "https://www.microsoft.com",
+        "excel": "https://excel.cloud.microsoft/?wdOrigin=APPHOME-WEB.DIRECT%2CAPPHOME-WEB.UNAUTH%2CAPPHOME-WEB.SHELL.SIGNIN",
+    }
+
+    apps = {
+        "notepad": "notepad.exe",
+        "calculator": "calc.exe",
+        "chrome": "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+        "firefox": "C:\\Program Files\\Mozilla Firefox\\firefox.exe",
+        "steam": "C:\\Program Files (x86)\\Steam\\Steam.exe",
+        "discord": "C:\\Users\\User\\AppData\\Local\\app-*\\Discord.exe",
+        "spotify": "C:\\Users\\EthanWinstead\\Library\\Caches\\com.spotify.client\\Spotify.exe",
+        "pycharm": "C:\\Program Files\\JetBrains\\PyCharm Community Edition 2024.3.4\\bin\\pycharm64.exe",
+        "jup": ["jupyter", "notebook"],
+    }
 
     while True:
         command = input("> ").strip().lower()
@@ -48,44 +79,19 @@ def Jarvis():
             if len(words) > 1:
                 site_or_app = ' '.join(words[1:]).lower()
 
-                # Dictionary for websites
-                websites = {
-                    "youtube": "https://www.youtube.com",
-                    "yt": "https://www.youtube.com",
-                    "google": "https://www.google.com",
-                    "gmail": "https://mail.google.com",
-                    "github": "https://github.com",
-                    "reddit": "https://www.reddit.com",
-                    "amazon": "https://www.amazon.com",
-                    "stackoverflow": "https://stackoverflow.com",
-                    "comptia": "https://login.comptia.org",
-                    "chess": "https://www.chess.com/play",
-                    "monkeytype": "https://monkeytype.com",
-                    "dictionary": "https://www.dictionary.com",
-                    "microsoft": "https://www.microsoft.com",
-                    "excel": "https://excel.cloud.microsoft/?wdOrigin=APPHOME-WEB.DIRECT%2CAPPHOME-WEB.UNAUTH%2CAPPHOME-WEB.SHELL.SIGNIN",           
-                }
-
-                # Dictionary for common applications
-                apps = {
-                    "notepad": "notepad.exe",
-                    "calculator": "calc.exe",
-                    "chrome": "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-                    "firefox": "C:\\Program Files\\Mozilla Firefox\\firefox.exe",
-                    "steam": "C:\\Program Files (x86)\\Steam\\Steam.exe",
-                    "discord": "C:\\Users\\User\\AppData\\Local\\app-*\\Discord.exe",
-                    "spotify": "C:\\Users\\EthanWinstead\\Library\\Caches\\com.spotify.client\\Spotify.exe",
-                    "pycharm": "C:\\Program Files\\JetBrains\\PyCharm Community Edition 2024.3.4\\bin\\pycharm64.exe",
-                }
-
                 if site_or_app in websites:
                     url = websites[site_or_app]
                     print(f"Gotcha Big boss opening {url}...")
                     webbrowser.open(url)
                 elif site_or_app in apps:
-                    app_path = apps[site_or_app]
+                    app_cmd = apps[site_or_app]
                     print(f"Gotcha Big boss opening {site_or_app}...")
-                    os.startfile(app_path)
+                    if isinstance(app_cmd, list):
+                        subprocess.Popen(app_cmd)  # commands like ["jupyter", "notebook"]
+                    elif app_cmd.startswith("http") or ".exe" in app_cmd:
+                        os.startfile(app_cmd)  # open a URL or a full path to an exe
+                    else:
+                        subprocess.Popen([app_cmd])  # simple one-word commands like "notepad"
                 else:
                     print(f"Sorry, I don't recognize '{site_or_app}'. Try using a full URL or specifying an installed application.")
             else:
@@ -109,10 +115,6 @@ def Jarvis():
 
         else:
             print("I'm sorry, I don't understand.")
-     
+
 if __name__ == "__main__":
     Jarvis()
-
-# In Jarvis.py, we have a chatbot function that can open a web browser, roll a die, and exit the program.
-# The chatbot function will keep asking questions until the user types "exit".
-# The chatbot function will open a web browser if the user types "open" followed by a URL.
